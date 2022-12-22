3963ba76c200d9389c8d39c41870ee166d654dc4
budko <budkovalexandr@gmail.com>
2022-12-22 19:25:46 +0300

    Вывеk значения каждой переменной в консоль.

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,13 @@ public class Main {
         var dog = 8;
         var cat = 3.6;
         var paper = 763789;
+//        System.out.println(dog);
+//        System.out.println(cat);
+//        System.out.println(paper);
+// Начало второй задачи
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
